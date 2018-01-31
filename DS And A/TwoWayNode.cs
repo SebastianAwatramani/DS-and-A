19b6149b2fc2980fd15c42b988861dd39cf2a43a
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2018-01-30 22:01:52 -0700

    Create TwoWayNode Class
    First steps of the TwoWayNodeClass

---
+++ DS And A/TwoWayNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_And_A
+{
+    class TwoWayNode : Node 
+    {
+        public TwoWayNode(int Data) : base(Data) { }
+            
+    }
+}
